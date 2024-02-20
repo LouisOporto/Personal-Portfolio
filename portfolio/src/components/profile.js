332f0d3fc7aa63b5fecb44f23bc69924a1e8da74
Louis Oporto <louisoporto@yahoo.com>
2024-02-20 13:17:19 -0800

    New content

--- portfolio/src/components/profile.js
+++ portfolio/src/components/profile.js
@@ -1,11 +1,11 @@
-
+import "../styles/profile.css";
 function Profile() {
   /* We will include content that is static to the profile. Photo, name, brief intro */
   return (
     <>
       <div className='profile'>
         <div id='photo'>
-          <img src={require("../assets/images/911_1996-1-3.jpg")} width='500px' height='500px' alt='Louis Oporto'></img>
+          
         </div>
         <div id='profile-content'>
           <h2>Louis Oporto</h2>

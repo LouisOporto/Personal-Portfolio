45c4d5d00651880441284a20dd0b1dae76ccd908
Louis Oporto <louisoporto@yahoo.com>
2024-02-21 10:05:06 -0800

    Sprint 3 - First Push

--- portfolio/src/components/navbar.js
+++ portfolio/src/components/navbar.js
@@ -4,10 +4,17 @@ function Navbar() {
   return (
     <>
       <div className="navbar">
-        <a href='#project'>Projects</a>
-        <a href='#work'>Work</a>
-        <a href='#about'>About</a>
-        <a href='#contact'>Contact</a>
+        <a className="links" href='#project'>Projects</a>
+        <a className="links" href='#work'>Work</a>
+        <a className="links" href='#about'>About</a>
+        <div class="dropdown">
+          <button class="dropbtn">Contact</button>
+          <div class="dropdown-content">
+            <a href="https://www.linkedin.com/in/louisoporto/" target="_blank">Linkedin</a>
+            <a href="https://github.com/LouisOporto" target="_blank">Github</a>
+            <a href="mailto:louisoporto203@gmail.com">Gmail</a>
+          </div>
+        </div>
       </div>
     </>
   );

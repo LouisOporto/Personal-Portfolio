59d65b4e229d78f95dddd985a5f2baf1f494177c
Louis Oporto <louisoporto@yahoo.com>
2024-02-20 23:31:24 -0800

    Second Sprint Complete

--- portfolio/src/components/profile.js
+++ portfolio/src/components/profile.js
@@ -3,18 +3,25 @@ function Profile() {
   /* We will include content that is static to the profile. Photo, name, brief intro */
   return (
     <>
-      <div className='profile'>
-        <div id='photo'>
-          
-        </div>
-        <div id='profile-content'>
-          <h2>Louis Oporto</h2>
-          <p>
-            This will be a paragraph about me
-          </p>
+      <div id="background">
+        <div className='profile'>
+          <div className='profile-section'>
+            <div id='photo'></div>
+          </div>
+          <div className='profile-section'>
+            <div id='profile-content'>
+              <h2>Louis Oporto</h2>
+              <p>
+                Building code line by line. Designing from the bottom up.<br/>
+                This will be a paragraph about me
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-      <img src={require("../assets/images/911_1996-1-3.jpg")}></img>
+      <div id='filler' />
+      <div id="background2"></div>
+      <div id='filler' />
     </>
   );
 }
